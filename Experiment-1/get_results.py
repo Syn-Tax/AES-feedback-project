1ede06d018a139c200784d1769900538acfbbb3d
Oscar Morris <twocap06@gmail.com>
2022-05-31 15:13:17 +0100

    finalized experiment 1

--- Experiment-1/get_results.py
+++ Experiment-1/get_results.py
@@ -129,8 +129,6 @@ def train():
         for batch in train_dataloader:
             batch = {k: v.to(device) for k, v in batch.items()}
             outputs = model(**batch)
-            print(outputs)
-            print(batch)
             loss = torch.nn.MSELoss()(outputs.logits, batch["labels"])
             loss.backward()
 
