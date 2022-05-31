ea6252629b3e3fec81b002099413096fbe4c9af3
Oscar Morris <twocap06@gmail.com>
2022-05-31 12:15:16 +0100

    finalized experiment 1

--- Experiment-1/get_results.py
+++ Experiment-1/get_results.py
@@ -1,113 +1,78 @@
+#!/usr/bin/env python3
+
 import pandas as pd
-from simpletransformers.classification import ClassificationArgs, ClassificationModel
-from IPython.display import clear_output
-import torch
-import logging
 import sklearn
 import numpy as np
+import torch
+import transformers
+import wandb
 import os
-import sys
-import json
 
-logging.basicConfig(level=logging.INFO)
-transformers_logger = logging.getLogger("transformers")
-transformers_logger.setLevel(logging.WARNING)
+wandb.login()
 
-# model_types = ["bert", "albert", "xlmroberta", "longformer"]
-model_types = ["bert"]
-# model_saves = ["roberta-base", "bert-base-cased", "albert-base-v2", "xlm-roberta-base", "allenai/longformer-base-4096"]
-model_saves = ["bert-base-cased"]
-# model_types = ["longformer"]
-# model_saves = ["allenai/longformer-base-4096"]
-# sample_sizes = [10, 20, 30, 40, 50]
-sample_sizes = [50]
-num_repeats = 1
-eval_size = 50
+os.environ["WANDB_ENTITY"] = "syntax483"
+os.environ["WANDB_PROJECT"] = "AES-Experiment-1"
 
-def train(model_type, model_save, sample_size, df, repeat):
-    wandb_config = {
-        "epochs": 100,
-        "train_batch_size": 8,
-        "eval_batch_size": 4,
-        "lr": 5e-5,
-        "samples": sample_size,
-        "max_seq_len": 512,
-        "model": model_type,
-        "save": model_save
-    }
+wandb_config = {
+    "epochs": 100,
+    "train_batch_size": 8,
+    "eval_batch_size": 4,
+    "lr": 1e-4,
+    "adam_beta_1": 0.9,
+    "adam_beta_2": 0.999,
+    "adam_epsilon": 1e-8,
+    "lr_scheduler": "linear",
+    "model": "bert",
+    "save": "bert-base-cased"
+}
 
+def load_data(train_size=50, eval_size=50):
+    df = pd.read_csv("/content/AES-feedback-project/Experiment-1/data.csv")
+    df = df.sample(frac=1).reset_index(drop=True)
 
-    train_df = df.iloc[:wandb_config["samples"], :]
-
+    train_df = df.iloc[:train_size]
     train_df.columns = ["text", "labels"]
 
-    eval_df = df.iloc[50:50+eval_size, :]
-
+    eval_df = df.iloc[eval_size:]
     eval_df.columns = ["text", "labels"]
 
-    model_args = ClassificationArgs()
-    model_args.num_train_epochs = wandb_config["epochs"]
-    model_args.eval_batch_size = wandb_config["eval_batch_size"]
-    model_args.train_batch_size = wandb_config["train_batch_size"]
-    model_args.wandb_project = "AES-Experiment-1"
-    model_args.wandb_kwargs = {"name": "{}-{}-{}".format(wandb_config["model"], wandb_config["samples"], repeat+1) }
-    model_args.learning_rate = wandb_config["lr"]
-    model_args.model = wandb_config["model"]
-    model_args.samples = wandb_config["samples"]
-    model_args.max_seq_length = wandb_config["max_seq_len"]
-    model_args.regression = True
-    model_args.no_save = True
-    model_args.overwrite_output_dir = True
-    model_args.logging_steps = np.ceil((wandb_config["samples"]/wandb_config["train_batch_size"]))
-    model_args.evaluate_during_training = True
-    # model_args.evaluate_during_training_verbose = True
-    model_args.evaluate_during_training_steps = np.ceil((wandb_config["samples"]/wandb_config["train_batch_size"])*10)
-    model_args.use_eval_cached_features = True
+    return train_df, eval_df
 
-    model = ClassificationModel(
-        wandb_config["model"],
-        wandb_config["save"],
-        num_labels=1,
-        args=model_args
+def configure_model(config):
+    return transformers.TrainingArguments(
+        output_dir="/content/output",
+        overwrite_output_dir=True,
+        evaluation_strategy="epoch",
+        learning_rate=config["lr"],
+        adam_beta1=config["adam_beta_1"],
+        adam_beta2=config["adam_beta_2"],
+        adam_epsilon=config["adam_epsilon"],
+        lr_scheduler_type=config["lr_scheduler"],
+        save_strategy="no"
     )
 
-    model.train_model(
-        train_df,
-        eval_df=eval_df,
-        mse=sklearn.metrics.mean_squared_error,
-        mae=sklearn.metrics.mean_absolute_error,
-        r2=sklearn.metrics.r2_score,
-        max=sklearn.metrics.max_error
+def tokenize(example, tokenizer):
+    return tokenizer(example["text"], padding="max_length", truncation=True)
+
+def train():
+    tokenizer = transformers.AutoTokenizer.from_pretrained(wandb_config["save"])
+
+    train_df, eval_df = load_data()
+    train_tokenized = tokenize(train_df, tokenizer)
+    eval_tokenized = tokenize(eval_df, tokenizer)
+
+    training_args = configure_model(wandb_config)
+
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(wandb_config["save"], num_labels=1)
+
+    train_dataloader = torch.utils.data.DataLoader(train_tokenized, batch_size=wandb_config["train_batch_size"])
+    eval_dataloader = torch.utils.data.DataLoader(eval_tokenized, batch_size=wandb_config["eval_batch_size"])
+
+    trainer = transformers.Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataloader,
+        eval_dataset=eval_dataloader
     )
 
-    result, model_outputs, wrong_predictions = model.eval_model(
-        eval_df,
-        mse=sklearn.metrics.mean_squared_error,
-        mae = sklearn.metrics.mean_absolute_error,
-        r2=sklearn.metrics.r2_score,
-        max=sklearn.metrics.max_error
-    )
-
-    return result, wrong_predictions, model_outputs
-
-def run():
-    results = []
-    for j in range(num_repeats):
-        df = pd.read_csv("/content/AES-feedback-project/Experiment-1/data.csv")
-        df = df.sample(frac=1).reset_index(drop=True)
-        result, wrong_predictions, model_outputs = train(model_types[0], model_saves[0], 50, df, j)
-        result["model"] = model_types[0]
-        result["sample size"] = 50
-        results.append(results)
-        torch.cuda.empty_cache()
-        clear_output()
-
-        errors = [[pred.text_a, float(pred.label), float(model_outputs[i])] for i, pred in enumerate(list(wrong_predictions))]
-        # errors_df = pd.DataFrame(errors).sort_values(1, ascending=False)
-        errors_df = pd.DataFrame(errors)
-        errors_df.to_csv(f"/content/drive/MyDrive/AES-feedback-project/Experiment-1/results/{model_types[0]}-{j}.csv")
-
-    return results
-
-if __name__ == "__main__":
-    results = run()
+    trainer.train()
