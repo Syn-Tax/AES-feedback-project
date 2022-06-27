a185aab09027b36f805aca6d5cd4f67abbb7bc39
Oscar Morris <twocap06@gmail.com>
2022-06-27 09:31:41 +0100

    added Experiment-6

---
+++ Experiment-6/model.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, output_size, vocab_size, embedding_length):
+        super().__init__()
+
+		self.output_size = output_size
+		self.vocab_size = vocab_size
