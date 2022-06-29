4410e125b0aa8c35fdb938703d443d04fe577770
Oscar Morris <twocap06@gmail.com>
2022-06-29 16:50:34 +0100

    added reports

--- Experiment-8/model.py
+++ Experiment-8/model.py
@@ -66,12 +66,13 @@ class Model(nn.Module):
 
 		input = self.word_embeddings(input_sentences)
 		input = input.permute(1, 0, 2)
-		print(input)
 		h_0 = Variable(torch.zeros(2, batch_size, self.hidden_size).cuda())
 		c_0 = Variable(torch.zeros(2, batch_size, self.hidden_size).cuda())
 
 		output, (h_n, c_n) = self.bilstm(input, (h_0, c_0))
 		output = output.permute(1, 0, 2)
+
+		print(output)
 
 		attn_weight_matrix = self.attention_net(output)
 		hidden_matrix = torch.bmm(attn_weight_matrix, output)
