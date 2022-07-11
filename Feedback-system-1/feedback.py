2a9bbf8529d9902b1b25cd33c2c9d0ccffdcbf88
Oscar Morris <twocap06@gmail.com>
2022-07-11 11:44:36 +0100

    added reports

--- Feedback-system-1/feedback.py
+++ Feedback-system-1/feedback.py
@@ -7,20 +7,14 @@ import sys
 from simpletransformers.classification import ClassificationModel
 from process_pdf import process_pdf
 import spacy
+from collections import Counter
 
 
-def main():
+def main(model, nlp):
     path = sys.argv[1]
-    model_path = "outputs/best_model"
-
-    nlp = spacy.load("en_core_web_lg")
-
-    model = ClassificationModel("bert", model_path)
-
     sample = process_pdf(path)
 
     tokens = nlp(sample["Abstract"])
-
     sentences = [sent.text.strip() for sent in tokens.sents]
     print()
     print()
@@ -32,5 +26,16 @@ def main():
     labels = ["BACKGROUND", "TECHNIQUE", "RESULT"]
     print([{sentences[i]: labels[predictions[i]]} for i in range(len(predictions))])
 
+    percs = {}
+    for key in Counter(predictions).keys():
+        percs[labels[key]] = dict(Counter(predictions))[key]/len(predictions)
+
+    print(percs)
+
 if __name__ == "__main__":
-    main()
+    model_path = "outputs/best_model"
+    model = ClassificationModel("bert", model_path)
+
+    nlp = spacy.load("en_core_web_lg")
+
+    main(model, nlp)
