64b8688be7bdb7e15674a5401bd38e2a96383264
Oscar Morris <twocap06@gmail.com>
2022-07-11 10:36:03 +0100

    added reports

---
+++ Feedback-system-1/feedback.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import pdfplumber
+import mark
+import sys
+from simpletransformers import ClassificationModel
+from process_pdf import process_pdf
+import spacy
+
+
+def main():
+    path = sys.argv[1]
+    model_path = "outputs/best_model"
+
+    nlp = spacy.load("en")
+
+    model = ClassificationModel("bert", model_path)
+
+    sample = process_pdf(path)
+
+    tokens = nlp(sample["Abstract"])
+
+    sentences = [sent.string.strip() for sent in tokens.sents]
+    print(sentences)
+    sys.exit(0)
+
+    predictions, raw_outputs = model.predict([sample["Abstract"]])
+
+if __name__ == "__main__":
+    main()
